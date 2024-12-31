39ab4b0ea5522f8cbbcb7e9df97aa0595c1cc561
Romus Shrestha <sromus498@gmail.com>
2024-12-31 13:38:10 +0545

    created client with different access permission

--- src/sanity/lib/backendClient.ts
+++ src/sanity/lib/backendClient.ts
@@ -7,7 +7,5 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  stega: {
-    studioUrl: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/studio` : `'http://${process.env.NEXT_PUBLIC_BASE_URL}/studio`
-  },
+  token: process.env.SANITY_API_TOKEN,
 })
