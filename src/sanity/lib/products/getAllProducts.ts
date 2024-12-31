c21d9d7e882a38a734c1bf2cacd7df20ba047b75
Romus Shrestha <sromus498@gmail.com>
2024-12-31 13:34:57 +0545

    created helper function to get all products

---
+++ src/sanity/lib/products/getAllProducts.ts
@@ -0,0 +1,15 @@
+import { defineQuery } from "next-sanity";
+import { sanityFetch } from "../live";
+
+export const getAllProducts = async () => {
+    const ALL_PRODUCTS_QUERY = defineQuery(
+        `*[_type=="product"]|order(name asc)`
+    );
+    try {
+        const product = await sanityFetch({ query: ALL_PRODUCTS_QUERY });
+        return product.data || []
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        return [];
+    }
+}
