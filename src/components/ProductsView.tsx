d72b6b9745d4a40fbee45ac4e240180a1b200326
Romus Shrestha <sromus498@gmail.com>
2024-12-31 13:37:36 +0545

    created components to display products

---
+++ src/components/ProductsView.tsx
@@ -0,0 +1,26 @@
+import { Category, Product } from "../../sanity.types";
+import ProductGrid from "./ProductGrid";
+
+interface IProductsViewProps {
+    products: Product[];
+    categories: Category[];
+}
+const ProductsView = ({ products, categories }: IProductsViewProps) => {
+    return (
+        <div className="flex flex-col">
+            {/* categories section */}
+            <div className="w-full sm:w-[200px]">
+                {/* <CategorySelectorComponent categories={categories} /> */}
+            </div>
+            {/* products section */}
+            <div className="flex-1">
+                <div>
+                    <ProductGrid products={products} />
+                    <hr className="w-1/2 sm:w-3/4" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsView;
